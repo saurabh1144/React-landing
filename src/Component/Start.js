4a5f18a79cd36acb7b00135566bcdee97cf2f5c7
saurabh1144 <saurabhchouragade706@gmail.com>
2024-02-15 00:48:35 +0530

    button created

--- src/Component/Start.js
+++ src/Component/Start.js
@@ -5,7 +5,14 @@ function Start() {
   return (
     <div className='container-fluid'>
     <Navbar/>
-    
+    <div className='container'>
+        <h2>Welcome to PopX</h2>
+        <label>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid voluptatibus, nulla inventore harum doloremque rerum deleniti eaque exercitationem tenetur! 
+        Qui nam eius est sapiente ipsa possimus tenetur explicabo? Quas, iure.</label>
+
+        <button className='btn btn-primary'>Create Account</button>
+        <button className='btn btn-primary'>Already Register ? Login</button>
+    </div>
     </div>
   )
 }
